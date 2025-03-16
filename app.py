f2cf50ae7591e0b092a3c8a3835253e72fde0d29
NeelChandwani1 <neelchandwani09@gmail.com>
2025-03-16 19:33:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def index():
     return render_template('index.html', plot_url=None)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
